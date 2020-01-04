4a31e92c66c5f0048149a61c90f5e1497afa8d5e
Kate Summerill <kaitlynsummerill@gmail.com>
2020-01-04 01:16:06 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -36,5 +36,13 @@ class TicTacToe
     @board[index] = character
   end
 
+  def position_taken?(array, index)
+  if array[index] == " " || array[index] == "" || array[index] == nil
+    return false
+  elsif array[index] == "X" || array[index] == "O"
+    return true
+  end
+end
+
 
 end
