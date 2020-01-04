8f1fb6e7f4ba629f8aa93390025717ef2b4bae74
Kate Summerill <kaitlynsummerill@gmail.com>
2020-01-04 01:33:54 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -67,4 +67,21 @@ class TicTacToe
       end
   end
 
+  def turn_count
+    # use each to iterate over the elements of the board
+    # to check if that element is an X or O and increment counter by 1
+    # return value of each is the original array
+    counter = 0
+    @board.each do |space_count|
+      if space_count == "X" || space_count == "O"
+          counter += 1
+          puts "#{space_count}"
+          # return counter
+      end
+    end
+    counter
+  end
+
+  
+
 end
