98d15d67b05a05bd9d717c7e76542d0cd5e61e17
Kate Summerill <kaitlynsummerill@gmail.com>
2020-01-04 01:07:51 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -16,9 +16,13 @@ class TicTacToe
     [6,4,2]
   ]
 
+  # board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   def display_board
-    # board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-    puts @board(\n)
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
   end
 
 
